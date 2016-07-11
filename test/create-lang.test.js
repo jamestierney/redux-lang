7aa0f00336e72905dfdf7dd42e3fddd624632a68
James Tierney <james@fifteendigital.co.uk>
2016-07-11 18:27:54 +0100

    chore(releasing): Add travis config, conventional commit, semantic-release
    closes #1

--- test/create-lang.test.js
+++ test/create-lang.test.js
@@ -1,10 +1,36 @@
 /* global describe it */
 /* eslint-env mocha */
 
-// import { expect } from 'chai'
-// import { createLang } from '../src/createLang'
+// import React from 'react'
+import { expect } from 'chai'
+import { createLang } from '../src/createLang'
 
-// describe('createLang', () => {
-//   it('should', () => {
-//   })
-// })
+const dictionary = {
+  en: {
+    home: {
+      welcome: 'Welcome'
+    }
+  },
+  fr: {
+    home: {
+      welcome: 'Bienvenue'
+    }
+  }
+}
+// const Component = ({ locale }) => {
+//   return <p>{locale}</p>
+// }
+
+let lang
+
+describe('createLang', () => {
+  it('should initialise the language decorator with the dictionary', () => {
+    lang = createLang(dictionary)
+    expect(lang('home')).to.be.a('function')
+  })
+  // it('should decorate the component', () => {
+  //   const NewComponent = lang('home')(Component)
+  //
+  //   // expect(lang('home')).to.be.a('function')
+  // })
+})
