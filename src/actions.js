7bb83c841dfc9863ed0480313693faa8bb989962
James Tierney <james@fifteendigital.co.uk>
2016-07-12 09:40:38 +0100

    fix(actions): remove addOne from actions
    closes #3

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,3 @@
 import { REDUX_LANG_SET_LOCALE } from './actionTypes'
 
 export const setLocale = value => ({ type: REDUX_LANG_SET_LOCALE, value })
-
-export const addOne = value => value + 1
--- test/actions.test.js
+++ test/actions.test.js
@@ -2,7 +2,7 @@
 /* eslint-env mocha */
 
 import { expect } from 'chai'
-import { setLocale, addOne } from '../src/actions'
+import { setLocale } from '../src/actions'
 import { REDUX_LANG_SET_LOCALE } from '../src/actionTypes'
 
 describe('actions', () => {
@@ -12,8 +12,5 @@ describe('actions', () => {
       value: 'fr'
     }
     expect(setLocale('fr')).to.eql(action)
-  })
-  it('should add one to my number', () => {
-    expect(addOne(5)).to.equal(6)
   })
 })
