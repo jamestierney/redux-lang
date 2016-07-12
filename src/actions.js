b338338810479a14fa3b84d3f119cca724eab72b
James Tierney <james@fifteendigital.co.uk>
2016-07-12 09:26:33 +0100

    test(actions): add tests to AddOne
    closes #4

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,5 @@
 import { REDUX_LANG_SET_LOCALE } from './actionTypes'
 
 export const setLocale = value => ({ type: REDUX_LANG_SET_LOCALE, value })
+
+export const addOne = value => value + 1
--- test/actions.test.js
+++ test/actions.test.js
@@ -2,7 +2,7 @@
 /* eslint-env mocha */
 
 import { expect } from 'chai'
-import { setLocale } from '../src/actions'
+import { setLocale, addOne } from '../src/actions'
 import { REDUX_LANG_SET_LOCALE } from '../src/actionTypes'
 
 describe('actions', () => {
@@ -12,5 +12,8 @@ describe('actions', () => {
       value: 'fr'
     }
     expect(setLocale('fr')).to.eql(action)
+  })
+  it('should add one to my number', () => {
+    expect(addOne(5)).to.equal(6)
   })
 })
